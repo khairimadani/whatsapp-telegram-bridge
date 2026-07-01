92ca0f1e2a59e3e0d73faced224ccb9bd59eb7a3
Khairi Madani <khairimadani16@gmail.com>
2026-07-02 01:12:38 +0800

    Fix channel and group sending

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,46 +1,46 @@
 import requests
 from config import BOT_TOKEN, CHANNEL_ID, GROUP_ID, GROUP_TOPIC_ID
+
 CHAT_IDS = [CHANNEL_ID, GROUP_ID]
 
 BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
 
-def send_message(text):
 
+def send_message(text):
     url = f"{BASE_URL}/sendMessage"
 
-    last_response = None
-
     for chat_id in CHAT_IDS:
-        last_response = requests.post(
-        url,
-        json={
-        "chat_id": chat_id,
-        "text": text
-        }
-    )
 
-    return last_response.json() if last_response else None
+        payload = {
+            "chat_id": chat_id,
+            "text": text
+        }
+
+        # Only add topic when sending to the group
+        if chat_id == GROUP_ID:
+            payload["message_thread_id"] = GROUP_TOPIC_ID
+
+        requests.post(url, json=payload)
+
 
 def send_photo(photo_path, caption=""):
-
     url = f"{BASE_URL}/sendPhoto"
-
-    last_response = None
 
     for chat_id in CHAT_IDS:
 
+        payload = {
+            "chat_id": chat_id,
+            "caption": caption
+        }
+
+        if chat_id == GROUP_ID:
+            payload["message_thread_id"] = GROUP_TOPIC_ID
+
         with open(photo_path, "rb") as photo:
-
-            last_response = requests.post(
-    url,
-    data={
-        "chat_id": chat_id,
-        "message_thread_id": GROUP_TOPIC_ID,
-        "caption": caption
-    },
-    files={
-        "photo": photo
-    }
-    )
-
-    return last_response.json() if last_response else None
+            requests.post(
+                url,
+                data=payload,
+                files={
+                    "photo": photo
+                }
+            )
