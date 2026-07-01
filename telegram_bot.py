c40e3bcf706575d2bf0f34b2ce46693075e8fec1
khairimadani <62692861+khairimadani@users.noreply.github.com>
2026-07-01 20:16:16 +0800

    Update bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,6 @@
 import requests
-from config import BOT_TOKEN, CHANNEL_ID
+from config import BOT_TOKEN, CHANNEL_ID, GROUP_ID
+CHAT_IDS = [CHANNEL_ID, GROUP_ID]
 
 BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
 
@@ -7,13 +8,15 @@ def send_message(text):
 
     url = f"{BASE_URL}/sendMessage"
 
-    response = requests.post(
-        url,
-        json={
-            "chat_id": CHANNEL_ID,
-            "text": text
-        }
-    )
+    for chat_id in CHAT_IDS:
+
+        requests.post(
+            url,
+            json={
+                "chat_id": chat_id,
+                "text": text
+            }
+        )
 
     return response.json()
 
@@ -21,17 +24,19 @@ def send_photo(photo_path, caption=""):
 
     url = f"{BASE_URL}/sendPhoto"
 
-    with open(photo_path, "rb") as photo:
+    for chat_id in CHAT_IDS:
 
-        response = requests.post(
-            url,
-            data={
-                "chat_id": CHANNEL_ID,
-                "caption": caption
-            },
-            files={
-                "photo": photo
-            }
-        )
+        with open(photo_path, "rb") as photo:
+
+            requests.post(
+                url,
+                data={
+                    "chat_id": chat_id,
+                    "caption": caption
+                },
+                files={
+                    "photo": photo
+                }
+            )
 
     return response.json()
